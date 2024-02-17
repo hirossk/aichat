5c91b235ab058df6272ff3a559e0243722e92a65
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2024-02-17 15:45:37 +0900

    simple librarymanaged

--- lm.py
+++ lm.py
@@ -6,15 +6,20 @@ app = Flask(__name__, static_folder="./static/")
 @app.route('/')
 def toppage():
     # トップページを表示する
-    return 'toppage'
+    return render_template('lm/login.html')
 
 @app.route('/signup')
 def signup():
     # トップページを表示する
     return render_template('lm/signup.html')
 
-@app.route('/regist', methods=['GET', 'POST'])
-def regist():
+@app.route('/bookadd')
+def books_add():
+    # トップページを表示する
+    return render_template('lm/bookadd.html')
+
+@app.route('/registuser', methods=['GET', 'POST'])
+def registuser():
     if request.method == 'GET':
         return render_template('lm/signup.html',message = '不正なアクセス（GETはダメ）')
     # トップページを表示する
@@ -25,6 +30,24 @@ def regist():
 
         return render_template('lm/login.html',message = '登録しました')
 
+@app.route('/registbook', methods=['GET', 'POST'])
+def registbook():
+    if request.method == 'GET':
+        return render_template('lm/bookadd.html',message = '不正なアクセス（GETはダメ）')
+    # トップページを表示する
+    else:
+        return render_template('lm/login.html',message = '登録しました')
+    
+@app.route('/booklist', methods=['GET', 'POST'])
+def booklist():
+    if request.method == 'GET':
+        return render_template('lm/booklist.html',message = '不正なアクセス（GETはダメ）')
+
+@app.route('/userlist', methods=['GET', 'POST'])
+def userlist():
+    if request.method == 'GET':
+        return render_template('lm/userlist.html',message = '不正なアクセス（GETはダメ）')
+        
 @app.route('/userpage')
 def userpage():
     # ユーザーページ（ダミー）
