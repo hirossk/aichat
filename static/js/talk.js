ea571142d217f83a3a4939658f9308e9d4d9c2b2
Yoshida Jobi <Gakuen@yoshida.com>
2024-05-05 20:46:02 +0900

    見栄えは完成したい

--- static/js/talk.js
+++ static/js/talk.js
@@ -13,6 +13,56 @@ window.onload = function () {
     }
 }
 
+function ajax(){
+    var $ta = $("#message-text")
+     // Ajax通信
+     $.ajax({
+        url: './call_ajax',
+        type: 'POST',
+        data: {
+            'sendmessage': $ta.val()
+        }
+    })
+        // Ajax通信が成功したら発動
+        .done((data) => {
+            var data_json = JSON.parse(data);
+            var face = data_json['face'];
+            var message = data_json['message'];
+            // $("#message_area").append("<div class=\"bubble right\"><img class=\"righticon\" src=\"/static/images/boy.png\" alt=\"\">" + message + "</div>");
+            $("#message_area").append("<div class=\"bubble right\"><div class=\"righticon\">" + face + "</div>" + message);
+            // $('.result').html(data);
+            var answer = data_json['answer'];
+            $("#message_area").append("<div class=\"bubble left\"><img class=\"lefticon\" src=\"/static/images/girl.png\" alt=\"\">" + answer + "</div>");
+        })
+        // Ajax通信が失敗したら発動
+        .fail((jqXHR, textStatus, errorThrown) => {
+            alert('Ajax通信に失敗しました。');
+            console.log("jqXHR          : " + jqXHR.status); // HTTPステータスを表示
+            console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラーなどのエラー情報を表示
+            console.log("errorThrown    : " + errorThrown.message); // 例外情報を表示
+        })
+        // Ajax通信が成功・失敗のどちらでも発動
+        .always((data) => {
+            if (document.getElementById("area")) {
+                // ↪︎ areaのIDがある場合に処理を実行させる（これがないとチャット画面がなくても常にJavaScriptが動いてしまいます）
+                var scrollHeight = document.getElementById("area").scrollHeight;
+                // ↪︎ area要素自体の最も高い場所を取得
+                let tgt;
+                if ('scrollingElement' in document) {
+                    tgt = document.scrollingElement;
+                } else if (this.browser.isWebKit) {
+                    tgt = document.body;
+                } else {
+                    tgt = document.documentElement;
+                }
+                tgt.scrollTop = scrollHeight;
+              }
+            $ta.val("");
+        });
+
+    //   $form.submit();
+}
+
 $(document).on("keydown", "#message-text", function (e) {
     var $ta = $("#message-text")
     if (e.keyCode == 13) { // Enterが押された
@@ -20,56 +70,17 @@ $(document).on("keydown", "#message-text", function (e) {
             $.noop();
         } else if ($ta.val().replace("/\s/g", "").length > 0) {
             e.preventDefault();
-
-            // Ajax通信
-            $.ajax({
-                url: './call_ajax',
-                type: 'POST',
-                data: {
-                    'sendmessage': $ta.val()
-                }
-            })
-                // Ajax通信が成功したら発動
-                .done((data) => {
-                    var data_json = JSON.parse(data);
-                    var face = data_json['face'];
-                    var message = data_json['message'];
-                    // $("#message_area").append("<div class=\"bubble right\"><img class=\"righticon\" src=\"/static/images/boy.png\" alt=\"\">" + message + "</div>");
-                    $("#message_area").append("<div class=\"bubble right\"><div class=\"righticon\">" + face + "</div>" + message);
-                    // $('.result').html(data);
-                    var answer = data_json['answer'];
-                    $("#message_area").append("<div class=\"bubble left\"><img class=\"lefticon\" src=\"/static/images/girl.png\" alt=\"\">" + answer + "</div>");
-                })
-                // Ajax通信が失敗したら発動
-                .fail((jqXHR, textStatus, errorThrown) => {
-                    alert('Ajax通信に失敗しました。');
-                    console.log("jqXHR          : " + jqXHR.status); // HTTPステータスを表示
-                    console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラーなどのエラー情報を表示
-                    console.log("errorThrown    : " + errorThrown.message); // 例外情報を表示
-                })
-                // Ajax通信が成功・失敗のどちらでも発動
-                .always((data) => {
-                    if (document.getElementById("area")) {
-                        // ↪︎ areaのIDがある場合に処理を実行させる（これがないとチャット画面がなくても常にJavaScriptが動いてしまいます）
-                        var scrollHeight = document.getElementById("area").scrollHeight;
-                        // ↪︎ area要素自体の最も高い場所を取得
-                        let tgt;
-                        if ('scrollingElement' in document) {
-                            tgt = document.scrollingElement;
-                        } else if (this.browser.isWebKit) {
-                            tgt = document.body;
-                        } else {
-                            tgt = document.documentElement;
-                        }
-                        tgt.scrollTop = scrollHeight;
-                      }
-                });
-            $ta.val("");
-            //   $form.submit();
+            ajax();
         }
     } else {
         $.noop();
     }
+});
 
-
+$(document).on("mousedown", "#submit", function (e) {
+    var $ta = $("#message-text")
+    if ($ta.val().replace("/\s/g", "").length > 0) {
+        e.preventDefault();
+        ajax();
+    }
 });
